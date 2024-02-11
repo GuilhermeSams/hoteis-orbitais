0397e84e59cd2ba89b3746522e4bc442b8a6ddca
GuilhermeSams <guilhermeps3670@gmail.com>
2024-02-11 15:48:56 -0300

    fix error seed

--- src/components/components-ui/ui/calendar.tsx
+++ src/components/components-ui/ui/calendar.tsx
@@ -14,6 +14,7 @@ function Calendar({
     className,
     classNames,
     showOutsideDays = true,
+    ...props
 }: CalendarProps) {
     return (
         <DayPicker
@@ -55,9 +56,14 @@ function Calendar({
                 ...classNames,
             }}
             components={{
-                IconLeft: ({}) => <ChevronLeft className="h-4 w-4" />,
-                IconRight: ({}) => <ChevronRight className="h-4 w-4" />,
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                IconRight: ({ ...props }) => (
+                    <ChevronRight className="h-4 w-4" />
+                ),
             }}
+            {...props}
         />
     )
 }
